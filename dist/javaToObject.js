86dea58285f2466eb4a36f2b17e758eb6e47cddf
Pavel Ivanoff <mail@example.com>
2016-12-20 12:58:31 +0000

    Separate java-objects functions

---
+++ dist/javaToObject.js
@@ -0,0 +1,35 @@
+/* global java */
+
+/*
+ * Converts Java ArrayList and/or HashMap set into native JavaSrcipt object
+ * @param {object} object
+ * @returns {object|array}
+ */
+function javaToObject(object) {
+    var result = null;
+
+    if (object instanceof java.util.ArrayList) {
+        result = [];
+        for (var i = 0; i < object.size(); i++) {
+            result.push(javaToObject(object.get(i)));
+        }
+    }
+    else if (object instanceof java.util.Date) {
+        result = new Date();
+        result.setTime(object.getTime());
+    }
+    else if (object instanceof java.util.HashMap) {
+        result = {};
+        var it = object.entrySet().iterator();
+        while (it.hasNext()) {
+            var pair = it.next();
+            var key = pair.getKey();
+            object[key] = javaToObject(pair.getValue());
+        }
+    }
+    else {
+        result = object;
+    }
+
+    return result;
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
 var gulp = require("gulp");
 
 var concat = require("gulp-concat");
-var strip = require('gulp-strip-comments');
+var clean = require("gulp-clean");
 
 gulp.task("default", function() {
     gulp.src("src/**/*.js")
         .pipe(gulp.dest("dist"))
         .pipe(concat("build.js"))
-        .pipe(gulp.dest("dist"))
+        .pipe(gulp.dest("dist/build"))
 })
 
-gulp.start("default");
 
 
